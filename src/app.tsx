3cc8b63bb0fe5741bfb38c6a119da6cb1979819e
Mike Jadick <mike@jadick.dev>
2021-01-16 14:36:39 -0700

    WIP - colors/typography/reset.css

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,8 @@
 import React, { FC, lazy } from 'react'
+import 'styles.css'
+import '@/public/reset.css'
+import '@/public/colors.css'
+import '@/public/typography.css'
 
 const LazyComponent = lazy(() => import('./components/Lazy/Lazy'))
 
@@ -7,6 +11,8 @@ const App: FC = () => {
     <div>
       <React.Suspense fallback={<h1>Loading</h1>}>
         <LazyComponent />
+        <p>Rest css is working</p>
+        <span className="font-size--default">Colors css is working</span>
       </React.Suspense>
     </div>
   )
