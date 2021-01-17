b64060255b01646c4a5c7aae20eada50d99bf5dd
Mike Jadick <mike@jadick.dev>
2021-01-17 12:41:20 -0700

    Integrate React.Router

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,21 @@
 import React, { FC, lazy } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import { HomePage } from '@components/pages/HomePage'
+// import { ProjectsPage } from '@components/pages/ProjectsPage'
+import { GitHubPage } from '@components/pages/GitHubPage'
 import 'styles.css'
 import '@/public/reset.css'
 import '@/public/colors.css'
 import '@/public/typography.css'
 
-const LazyComponent = lazy(() => import('./components/Lazy/Lazy'))
+// const LazyComponent = lazy(() => import('./components/Lazy/Lazy'))
 
 const App: FC = () => {
   return (
-    <div>
-      <React.Suspense fallback={<h1>Loading</h1>}>
-        <LazyComponent />
-        <p>Rest css is working</p>
-        <span className="font-size--default">Colors css is working</span>
-      </React.Suspense>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ HomePage } />
+      <Route path="/:id" component={ GitHubPage } />
+    </Switch>
   )
 }
 
