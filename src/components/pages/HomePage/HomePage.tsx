08e1fdb746161c8de09686a1fb5078e583c22f4d
Mike Jadick <mike@jadick.dev>
2021-03-13 14:04:12 -0700

    Remove unused imports

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -2,9 +2,7 @@ import React, { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { HeroCard } from '@components/utility/HeroCard'
 import './HomePage.styles.css'
-import 'styles.css'
 
-const onGitHub = ' on Github'
 
 const heroHeader = 'I\'m Michael Jadick'
 const heroSubheaderLine1 = 'I\'m a software engineer specializing in React and Typescript.'
