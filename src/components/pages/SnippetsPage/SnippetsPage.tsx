8ea59f6ebfa9c826107e3ed8a4cd14e29b2a1a3b
Mike Jadick <mike@jadick.dev>
2021-03-13 14:27:39 -0700

    Prettier SnippetsPage

--- src/components/pages/SnippetsPage/SnippetsPage.tsx
+++ src/components/pages/SnippetsPage/SnippetsPage.tsx
@@ -7,18 +7,12 @@ const heroHeader = 'Snippets'
 const heroSubheaderLine1 = null
 const heroSubheaderLine2 = null
 
-
-
 export const SnippetsPage: FC = () => {
   const { t } = useTranslation()
 
   return (
     <section className="home-page__hero-section">
-      <HeroCard
-        header={ heroHeader }
-        subheaderLine1={ heroSubheaderLine1 }
-        subheaderLine2={ heroSubheaderLine2 }
-      />
+      <HeroCard header={heroHeader} subheaderLine1={heroSubheaderLine1} subheaderLine2={heroSubheaderLine2} />
     </section>
   )
 }
