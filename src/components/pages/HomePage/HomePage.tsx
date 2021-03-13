5d624e00649666f5461efa34ba98f03a18a44a07
Mike Jadick <mike@jadick.dev>
2021-03-13 14:27:06 -0700

    Prettier HomePage

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -3,23 +3,16 @@ import { useTranslation } from 'react-i18next'
 import { HeroCard } from '@components/utility/HeroCard'
 import './HomePage.styles.css'
 
-
-const heroHeader = 'I\'m Michael Jadick'
-const heroSubheaderLine1 = 'I\'m a software engineer specializing in React and Typescript.'
-const heroSubheaderLine2 = 'This website is a collection of the things I\'ve cultivated and created over the years.'
-
-
+const heroHeader = "I'm Michael Jadick"
+const heroSubheaderLine1 = "I'm a software engineer specializing in React and Typescript."
+const heroSubheaderLine2 = "This website is a collection of the things I've cultivated and created over the years."
 
 export const HomePage: FC = () => {
   const { t } = useTranslation()
 
   return (
     <section className="home-page__hero-section">
-      <HeroCard
-        header={ heroHeader }
-        subheaderLine1={ heroSubheaderLine1 }
-        subheaderLine2={ heroSubheaderLine2 }
-      />
+      <HeroCard header={heroHeader} subheaderLine1={heroSubheaderLine1} subheaderLine2={heroSubheaderLine2} />
     </section>
   )
 }
