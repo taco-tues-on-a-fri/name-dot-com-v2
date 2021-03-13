cc16fe80a84151d0e3d7b064e2be9fa0ea5815e4
Mike Jadick <mike@jadick.dev>
2021-03-13 14:03:10 -0700

    Create SnippetsPage

---
+++ src/components/pages/SnippetsPage/SnippetsPage.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+import { HeroCard } from '@components/utility/HeroCard'
+import './SnippetsPage.styles.css'
+
+const heroHeader = 'Snippets'
+const heroSubheaderLine1 = null
+const heroSubheaderLine2 = null
+
+
+
+export const SnippetsPage: FC = () => {
+  const { t } = useTranslation()
+
+  return (
+    <section className="home-page__hero-section">
+      <HeroCard
+        header={ heroHeader }
+        subheaderLine1={ heroSubheaderLine1 }
+        subheaderLine2={ heroSubheaderLine2 }
+      />
+    </section>
+  )
+}
