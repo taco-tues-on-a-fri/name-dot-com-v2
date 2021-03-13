3b40748053f56178a3150701de56f363382ae1ff
Mike Jadick <mike@jadick.dev>
2021-03-13 14:24:57 -0700

    Prettier AboutPage

--- src/components/pages/AboutPage/AboutPage.tsx
+++ src/components/pages/AboutPage/AboutPage.tsx
@@ -3,23 +3,16 @@ import { useTranslation } from 'react-i18next'
 import { HeroCard } from '@components/utility/HeroCard'
 import './AboutPage.styles.css'
 
-
 const heroHeader = 'About'
 const heroSubheaderLine1 = null
 const heroSubheaderLine2 = null
-
-
 
 export const AboutPage: FC = () => {
   const { t } = useTranslation()
 
   return (
     <section className="home-page__hero-section">
-      <HeroCard
-        header={ heroHeader }
-        subheaderLine1={ heroSubheaderLine1 }
-        subheaderLine2={ heroSubheaderLine2 }
-      />
+      <HeroCard header={heroHeader} subheaderLine1={heroSubheaderLine1} subheaderLine2={heroSubheaderLine2} />
     </section>
   )
 }
