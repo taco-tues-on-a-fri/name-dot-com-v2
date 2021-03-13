b300ceff6c58f6e1833bd41508429cce17fe8fbb
Mike Jadick <mike@jadick.dev>
2021-03-13 14:25:17 -0700

    Prettier ContactPage

--- src/components/pages/ContactPage/ContactPage.tsx
+++ src/components/pages/ContactPage/ContactPage.tsx
@@ -3,23 +3,16 @@ import { useTranslation } from 'react-i18next'
 import { HeroCard } from '@components/utility/HeroCard'
 import './ContactPage.styles.css'
 
-
 const heroHeader = 'Contact'
 const heroSubheaderLine1 = null
 const heroSubheaderLine2 = null
-
-
 
 export const ContactPage: FC = () => {
   const { t } = useTranslation()
 
   return (
     <section className="home-page__hero-section">
-      <HeroCard
-        header={ heroHeader }
-        subheaderLine1={ heroSubheaderLine1 }
-        subheaderLine2={ heroSubheaderLine2 }
-      />
+      <HeroCard header={heroHeader} subheaderLine1={heroSubheaderLine1} subheaderLine2={heroSubheaderLine2} />
     </section>
   )
 }
