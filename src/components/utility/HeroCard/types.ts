5ac5421011f7833dbc49a059049d5ff152e76e31
Mike Jadick <mike@jadick.dev>
2021-03-14 16:16:13 -0600

    Add CreateButton component to HeroCard

--- src/components/utility/HeroCard/types.ts
+++ src/components/utility/HeroCard/types.ts
@@ -2,4 +2,16 @@ export type HeroCardProps = {
   header: string
   subheaderLine1?: string | null
   subheaderLine2?: string | null
+  buttonConfigArrayOfObjects?: ButtonConfigObject[] | null
+}
+
+export type ButtonConfigObject = {
+  id: string
+  path: string
+  label: string
+  href?: string
+}
+
+export type CreateButtonsProps = {
+  configArrayOfObjects: ButtonConfigObject[]
 }
