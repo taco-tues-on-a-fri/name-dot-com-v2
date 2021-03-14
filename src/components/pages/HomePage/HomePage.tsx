5ca1c3a06b6b973d17daff7a3019a92573cd735c
Mike Jadick <mike@jadick.dev>
2021-03-14 16:15:15 -0600

    Cleanup HomePage

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,18 +1,42 @@
 import React, { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { HeroCard } from '@components/utility/HeroCard'
+import { GITHUB_PROFILE_URL } from '@lib/constants'
 import './HomePage.styles.css'
+import stringJson from '@i18n/locales/en/home.json'
 
-const heroHeader = "I'm Michael Jadick"
-const heroSubheaderLine1 = "I'm a software engineer specializing in React and Typescript."
-const heroSubheaderLine2 = "This website is a collection of the things I've cultivated and created over the years."
+const buttonConfigArrayOfObjects = [
+  {
+    id: 'hero-button__about',
+    path: '/about',
+    label: 'About Me',
+    href: undefined,
+  },
+  {
+    id: 'hero-button__snippets',
+    path: '/snippets',
+    label: 'Snippets',
+    href: undefined,
+  },
+  {
+    id: 'hero-button__github',
+    path: '',
+    label: 'GitHub',
+    href: GITHUB_PROFILE_URL,
+  },
+]
 
 export const HomePage: FC = () => {
   const { t } = useTranslation()
 
   return (
-    <section className="home-page__hero-section">
-      <HeroCard header={heroHeader} subheaderLine1={heroSubheaderLine1} subheaderLine2={heroSubheaderLine2} />
+    <section className="page__hero-section">
+      <HeroCard
+        header={stringJson.heroHeader}
+        subheaderLine1={stringJson.heroSubheaderLine1}
+        subheaderLine2={stringJson.heroSubheaderLine2}
+        buttonConfigArrayOfObjects={buttonConfigArrayOfObjects}
+      />
     </section>
   )
 }
