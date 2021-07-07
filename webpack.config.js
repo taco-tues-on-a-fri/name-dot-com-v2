3c589bcee54dc3f4643a19696fb3788ec3ebd206
Mike Jadick <mike@jadick.dev>
2021-07-07 10:45:27 -0600

    Fix hot reloading issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path')
-const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ReactRefreshTypeScript = require('react-refresh-typescript')
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -27,10 +28,20 @@ const tsConfig = {
   exclude: PATH.nodeModules,
   use: [
     isDevelopment && {
-      loader: 'babel-loader',
-      options: { plugins: ['react-refresh/babel'] },
+      loader: require.resolve('babel-loader'),
+      options: { 
+        plugins: [
+          isDevelopment && require.resolve('react-refresh/babel'),
+        ] },
     },
-    'ts-loader',
+    {
+      loader: require.resolve('ts-loader'),
+      options: {
+        getCustomTransformers: () => ({
+          before: isDevelopment ? [ReactRefreshTypeScript()] : [],
+        }),
+      },
+    }
   ].filter(Boolean),
 }
 
@@ -82,7 +93,6 @@ module.exports = {
     rules: [tsConfig, htmlConfig, cssConfig],
   },
   plugins: [
-    isDevelopment && new ReactRefreshPlugin(),
     new HtmlWebpackPlugin({
       filename: './index.html',
       template: './public/index.html',
@@ -105,7 +115,9 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: isDevelopment ? '[name].css' : '[name].[hash].css',
       chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
-    })
+    }),
+    isDevelopment && new webpack.HotModuleReplacementPlugin(),
+    isDevelopment && new ReactRefreshWebpackPlugin()
   ].filter(Boolean),
   cache: true,
   bail: false,
@@ -116,6 +128,7 @@ module.exports = {
     contentBase: './dist',
     historyApiFallback: true
   },
+  target: 'web',
   stats: 'errors-only',
   performance: {
     hints: false
